4c2fda0831c64a006cee8e6ccb9f0bc27bab35a3
Hariom kankatti <70455169+hk973@users.noreply.github.com>
2024-02-25 03:20:21 +0530

    Update constants.kt
    by mistake personal key got public

--- app/src/main/java/com/samrudhasolutions/bolo/utils/constants.kt
+++ app/src/main/java/com/samrudhasolutions/bolo/utils/constants.kt
@@ -2,6 +2,6 @@ package com.samrudhasolutions.bolo.utils
 
 const val BASE_URL="https://api.openai.com/v1/"
 const val CHATGPT_MODEL="gpt-3.5-turbo"
-const val OPENAI_API_KEY="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"
+const val OPENAI_API_KEY="write your api key"
 var ANSBEFOREEDIT=""
 var ANSWERAFTEREDIT=""
